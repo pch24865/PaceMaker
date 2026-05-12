8d69951a606a316e7e91c6b239f678d7c619a543
pch24865 <pch24865@gmail.com>
2026-05-12 09:30:30 +0900

    api 인터셉트문제 무한새로고침 수정

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -103,7 +103,9 @@ api.interceptors.response.use(
     (response) => response,
     (error) => {
         if (error.response && error.response.status === 401) {
-            window.location.href = "/signin";
+            if (error.config && error.config.url !== '/api/auth/me' && window.location.pathname !== '/signin' && window.location.pathname !== '/signup') {
+                window.location.href = "/signin";
+            }
         }
         return Promise.reject(error);
     }
